c9267f505db3d6c9e6b5979632bf466eb3807059
跛豪 <142658136+lee388366@users.noreply.github.com>
2026-03-02 05:19:04 +0800

    创建 run_all.sql
    Add run_all.sql

---
+++ mimic-ccw-crrt-septicshock-aki/run_all.sql
@@ -0,0 +1,10 @@
+\i mimic-ccw-crrt-septicshock-aki/sql/01_cohort_eligibility_ss.sql
+\i mimic-ccw-crrt-septicshock-aki/sql/02_define_t0_kdigo3_ss.sql
+\i mimic-ccw-crrt-septicshock-aki/sql/03_apply_septic_shock_and_exclude_rrt_pre_t0_ss.sql
+
+\i mimic-ccw-crrt-septicshock-aki/sql/05_baseline_table1_v1.sql
+\i mimic-ccw-crrt-septicshock-aki/sql/06_ccw_long_0_24h_dynamic_1h.sql
+\i mimic-ccw-crrt-septicshock-aki/sql/08_ccw_clone_censor_1h_with_admin.sql
+\i mimic-ccw-crrt-septicshock-aki/sql/07_outcomes_28d_renal_recovery_scr.sql
+
+\i mimic-ccw-crrt-septicshock-aki/sql/00_attrition_counts_flow.sql

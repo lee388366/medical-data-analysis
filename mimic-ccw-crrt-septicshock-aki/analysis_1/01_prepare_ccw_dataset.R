a6ff937d5f22c8a5ea195f3a455d4846dfb8955a
跛豪 <142658136+lee388366@users.noreply.github.com>
2026-03-05 10:04:32 +0800

    Update 01_prepare_ccw_dataset.R

--- mimic-ccw-crrt-septicshock-aki/analysis_1/01_prepare_ccw_dataset.R
+++ mimic-ccw-crrt-septicshock-aki/analysis_1/01_prepare_ccw_dataset.R
@@ -1,16 +1,38 @@
-#808 + 505 baseline
-df_long <- dbGetQuery(con,"
+library(DBI)
+library(dplyr)
+
+# database connection
+con <- dbConnect(RPostgres::Postgres(),
+                 dbname = "mimic")
+
+# clone–censor dataset
+df_clone <- dbGetQuery(con, "
 SELECT *
 FROM data_extract_crrt.ccw_clone_long_0_24h_1h_v2
 ")
 
-df_baseline <- dbGetQuery(con,"
-SELECT stay_id,
-       weight_kg,
-       ne_eq_baseline,
-       mv_baseline
+# baseline variables
+df_baseline <- dbGetQuery(con, "
+SELECT
+    stay_id,
+    age,
+    sex,
+    weight_kg,
+    charlson_comorbidity_index,
+    ne_eq_baseline,
+    mv_baseline,
+    pfratio,
+    lactate,
+    ph,
+    scr_baseline_win,
+    potassium
 FROM data_extract_crrt.cohort_baseline_v1
 ")
 
-df <- df_long %>%
-  left_join(df_baseline, by="stay_id")
+# merge baseline with long dataset
+df <- df_clone %>%
+  left_join(df_baseline, by = "stay_id")
+
+# keep only rows at risk
+df <- df %>%
+  filter(at_risk_after_k == 1)
